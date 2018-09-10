8f0296a761f2bfc4883d9b6a5d9da17e90206dd3
Pascal Kaufmann <kaufmae@me.com>
2018-09-10 20:19:44 +0200

    reset builds

--- add-version.js
+++ add-version.js
@@ -8,6 +8,14 @@ const dockerfile = fs
   .replace('{{METEOR_VERSION}}', version)
 
 fs.mkdirSync(`${folder}/${version}`)
-fs.unlinkSync(`${folder}/latest/Dockerfile`)
-fs.writeFileSync(`${folder}/latest/Dockerfile`, dockerfile)
 fs.writeFileSync(`${folder}/${version}/Dockerfile`, dockerfile)
+
+if (
+  version.indexOf('rc') === -1 &&
+  version.indexOf('canary') === -1 &&
+  version.indexOf('beta') === -1 &&
+  version.indexOf('alpha') === -1) {
+  // official release, update latest
+  fs.unlinkSync(`${folder}/latest/Dockerfile`)
+  fs.writeFileSync(`${folder}/latest/Dockerfile`, dockerfile)
+}
